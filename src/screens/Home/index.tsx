ccc4c7fac8d12fa21dfd6e16922f30450d6566ea
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2023-01-18 01:47:34 -0300

    components

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
+import { Participant } from '../../components/Participant';
 export function Home() {
 
   function handleParticipantAdd() {
@@ -27,6 +28,8 @@ export function Home() {
           </Text>
         </TouchableOpacity>
       </View>
+      <Participant />
+      <Participant />
     </View>
   )
 }
